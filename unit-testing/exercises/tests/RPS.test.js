9647564d1e004810736a80448a1d8fccdc9f5619
Christopher LaCoste <clacoste994@gmail.com>
2023-11-06 18:59:25 -0600

    unittesthw

--- unit-testing/exercises/tests/RPS.test.js
+++ unit-testing/exercises/tests/RPS.test.js
@@ -8,14 +8,14 @@ describe("whoWon", function() {
     test("Result of", function() {
         let outPut2 = whoWon('rock', 'paper');
         expect(outPut2).toBe('Player 2 wins!');
-    })
+    });
     test("Result of", function() {
         let outPut3 = whoWon('paper', 'scissors');
         expect(outPut3).toBe('Player 2 wins!');
-    })
+    });
     test("Result of",function() {
         let outPut4 = whoWon('scissors', 'rock');
         expect(outPut4).toBe('Player 2 wins!');
-    })
+    });
     
 });
--- unit-testing/exercises/tests/checkFive.test.js
+++ unit-testing/exercises/tests/checkFive.test.js
@@ -7,3 +7,4 @@ describe("checkFive", function() {
     });
 });
 
+//describe
