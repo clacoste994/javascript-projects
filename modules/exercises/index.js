67b78e993012a1f97af5a77be05745e09af4700e
Christopher LaCoste <clacoste994@gmail.com>
2024-02-04 19:45:49 -0600

    exercises

--- modules/exercises/index.js
+++ modules/exercises/index.js
@@ -1,8 +1,9 @@
 //Import modules:
-const input = //Import readline-sync.
-const averages = //Import functions from averages.js.
-const printAll = //Import function from display.js.
-const randomSelect = //Import function from randomSelect.js.
+const input = require('readline-sync');
+const averages = require('./averages.js');
+const printAll = require('./display.js');
+const randomSelect = require('./randomSelect');
+
 
 //Candidate data:
 let astronauts = ['Fox','Turtle','Cat','Hippo','Dog'];
@@ -11,6 +12,8 @@ const testTitles = ['Math','Fitness','Coding','Nav','Communication'];
 
 let scores = [[95, 86, 83, 81, 76],[79, 71, 79, 87, 72],[94, 87, 87, 83, 82],[99, 77, 91, 79, 80],[96, 95, 99, 82, 70]];
 
+randomSelect = randomFromArray(arr);
+
 //User interface:
 let prompts = ['display all scores', 'average the scores for each test', 'average the scores for each astronaut','select the next spacewalker'];
 
@@ -18,19 +21,19 @@ for (let i = 0; i<prompts.length; i++){
   let response = input.question(`Would you like to ${prompts[i]}? Y/N: `);
   if (response.toLowerCase()==='y'){
     if (i===0){
-      //Call 'printAll' here and pass in all necessary arguments.
+      printAll(astronauts,testTitles,scores);
     } else if (i===1){
       for (let j = 0; j<testTitles.length; j++){
-        let avg = //Call 'averageForTest' here. Pass in j and scores as arguments.
+        let avg = averages.averageForStudent(j, scores);
         console.log(`${testTitles[j]} test average = ${avg}%.`);
       }
     } else if (i===2){
       for (let j = 0; j<astronauts.length; j++){
-        let avg = //Call 'averageForStudent' here. Pass in j and scores as arguments.
+        let avg = averages.averageForStudent(j, scores);
         console.log(`${astronauts[j]}'s test average = ${avg}%.`);
       }
     } else {
-      let walker = //Call 'randomSelect' to pick a spacewalker from the astronauts array.
+      let walker = randomFromArray(astronauts);
       console.log(`${walker} is the next spacewalker.`);
     }
   } else {
