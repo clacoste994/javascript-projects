e852be9f68d00e9f10cb6a153127be07e23c4697
Christopher LaCoste <clacoste994@gmail.com>
2024-02-13 18:34:39 -0600

    studio

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -1,6 +1,16 @@
 const input = require('readline-sync');
 let str = "LaunchCode";
 
+let str2 = str.slice(0,3);
+
+//onsole.log(str2);
+
+let str3 = str.slice(3,10)
+
+let pigLaunchCode = str3 + str2;
+
+//console.log(pigLaunchCode);
+
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
 
@@ -8,4 +18,12 @@ let str = "LaunchCode";
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
+let userStuff = input.question("Number???  ");
+
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+
+if(userStuff > str.length) {
+    console.log(pigLaunchCode);
+}
+
+console.log(userStuff);
