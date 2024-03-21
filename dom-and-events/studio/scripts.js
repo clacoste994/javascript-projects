e7314e4150532c10579ae4ed1ba262f0b97418b3
Christopher LaCoste <clacoste994@gmail.com>
2024-03-21 18:53:14 -0500

    domStudio

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -1,2 +1,33 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener('load', function() {
+
+takeOffButton.addEventListener('click', function() {
+
+})
+
+landButton.addEventListener('click', function() {
+
+})
+
+abortMissionButton.addEventListener('click', function() {
+
+})
+
+upButton.addEventListener('click', function() {
+
+})
+
+downButton.addEvenListener('click', function() {
+
+})
+
+rightButton.addEventListener('click', function() {
+
+})
+
+leftButton.addEventListener('click', function() {
+    
+})
+
+})
